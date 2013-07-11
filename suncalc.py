843f48a9aedefaaf6969245d9e4618cebc45884e
John McKenzie <jmckind@gmail.com>
2013-07-11 17:39:20 -0500

    Add support for multiple observer locations

--- suncalc.py
+++ suncalc.py
@@ -1,18 +1,38 @@
 from ephem import *
 
-san_antonio = Observer()
-san_antonio.lat = '29.416667'
-san_antonio.lon = '-98.5'
+locations = {
+	'Dallas/Ft. Worth': ('32.775833', '-96.796667'),
+	'London': ('51.507222', '-0.1275'),
+	'Hong Kong': ('22.24819', '114.20340'),
+	'Australia': ('-23.484168', '134.119034'),
+	'Los Angeles': ('34.05', '-118.25'),
+	'New York': ('40.67', '-73.94'),
+}
+
+observers = dict()
+
+for location in locations:
+	coordinates = locations.get(location)
+
+	o = Observer()
+	o.lat, o.lon = coordinates
+
+	observers[location] = o
 
 sun = Sun()
-sun.compute(san_antonio)
+	
+for observer_name in observers:
+	observer = observers[observer_name]
 
-print "The sun's azimuth is %s. The sun's altitude is %s." % (sun.az, sun.alt)
+	sun.compute(observer)
 
-local_sunrise = localtime(san_antonio.next_rising(sun)).strftime('%I:%M %p')
-local_sunset = localtime(san_antonio.next_setting(sun)).strftime('%I:%M %p')
+	print observer_name
+	print "\tAltitude: %s Azimuth: %s" % (sun.alt, sun.az)
 
-if local_sunrise > local_sunset:
-	print "Today's sunset is at %s. Tomorrow's sunrise is at %s." % (local_sunset, local_sunrise)
-else:
-	print "Today's sunrise is at %s. Today's sunset is at %s. " % (local_sunrise, local_sunset)
+	local_sunrise = localtime(observer.next_rising(sun)).strftime('%I:%M %p')
+	local_sunset = localtime(observer.next_setting(sun)).strftime('%I:%M %p')
+
+	if local_sunrise > local_sunset:
+		print "\tToday's sunset is at %s. Tomorrow's sunrise is at %s." % (local_sunset, local_sunrise)
+	else:
+		print "\tToday's sunrise is at %s. Today's sunset is at %s. " % (local_sunrise, local_sunset)
